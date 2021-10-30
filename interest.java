b906eaf6f402912dfc24f5c0c76277d20492a1b3
STRK-ND <raj15400881@gmail.com>
2021-10-30 10:13:12 +0530

    name

--- interest.java
+++ interest.java
@@ -6,12 +6,12 @@ public class interest {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the Principal : ");
         p = scan.nextFloat();
-        System.out.println("Enter the Rate of intrest : ");
+        System.out.println("Enter the Rate of interest : ");
         r = scan.nextFloat();
         System.out.println("Enter the Time period : ");
         t = scan.nextFloat();
         scan.close();
         sinterest = (p * r * t) / 100;
-        System.out.println("Simple Intrest is: " + sinterest);
+        System.out.println("Simple Interest is: " + sinterest);
     }
 }
